1e59a8fbf25d052ba39d0422265e1d69d39ed25d
Sondre Halvorsen <Sondre.Halvorsen@nav.no>
2021-03-27 14:22:08 +0100

    Implement several auth flow decisions
    Session cookie present but no valid token i cache
    Session cookie present, no valid token, but code in query

--- src/oauther.rs
+++ src/oauther.rs
@@ -1,17 +1,17 @@
 use crate::{FilterConfig, util};
-use oauth2::{ClientSecret, ClientId, TokenUrl, PkceCodeChallenge, AuthUrl, RedirectUrl, CsrfToken, Scope, PkceCodeVerifier};
+use oauth2::{ClientSecret, ClientId, TokenUrl, PkceCodeChallenge, AuthUrl, RedirectUrl, CsrfToken, Scope, PkceCodeVerifier, HttpRequest, AuthType};
 use url;
 use cookie::{Cookie, CookieBuilder};
 use crate::oauther::Response::{NewAction, NewState};
 use url::{Url, ParseError};
 use oauth2::basic::BasicClient;
 use getrandom;
-use std::cell::RefCell;
-use std::ops::DerefMut;
+
 use oauth2::http::{HeaderMap, HeaderValue};
 use std::time;
 use std::time::Duration;
-use oauth2::http::header::SET_COOKIE;
+use oauth2::http::header::{SET_COOKIE, HeaderName, AUTHORIZATION};
+
 
 pub struct OAuther {
     config: OAutherConfig,
@@ -23,12 +23,8 @@ pub struct OAuther {
 pub enum Action {
     Noop,
     Redirect(Url, HeaderMap),
-    HttpCall,
-    Allow
-}
-
-trait State {
-    fn handle_request(&self, oauther: &OAuther, header: &Vec<(&str, &str)>) -> Response;
+    HttpCall(HttpRequest),
+    Allow(HeaderMap)
 }
 
 pub trait Cache {
@@ -37,6 +33,10 @@ pub trait Cache {
 
     fn get_verfier_for_state(&self, state: &String) -> Option<&PkceCodeVerifier>;
     fn set_verfier_for_state(&mut self, state: &String, verifier: &PkceCodeVerifier);
+}
+
+trait State {
+    fn handle_request(&self, oauther: &OAuther, header: &Vec<(&str, &str)>) -> Response;
 }
 
 impl OAuther {
@@ -71,10 +71,15 @@ impl OAuther {
             }
             Response::NewAction(action) => match action {
                 OAutherAction::Redirect(url, headers, update) => {
-                    update(self);
+                    update(self); // run the mutating update
                     Action::Redirect(url, headers)
                 }
-                _ => Action::Noop,
+                OAutherAction::Allow(headers) => {
+                    Action::Allow(headers)
+                },
+                OAutherAction::HttpCall(request) =>
+                    Action::HttpCall(request),
+                _ => unreachable!(),
             }
         }
     }
@@ -130,6 +135,46 @@ impl OAuther {
                 move |  oauther|
                     { oauther.cache.set_verfier_for_state(&closure_state, &closure_verifier)}))
     }
+
+    fn allow_headers(&self, token: &String) -> HeaderMap {
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            AUTHORIZATION,
+            HeaderValue::from_str(format!("bearer {}",token).as_str()).unwrap());
+        headers
+    }
+
+    fn request_auth_code(&self, headers: &Vec<(&str, &str)>) -> Option<String> {
+        // TODO this could ben done easier, without needing authority, without lib support
+        let authority =
+            headers.iter().find( |(name, value)| { *name == ":authority"}).map( | entry|{ entry.1 });
+        let path =
+            headers.iter().find( |(name, value)| { *name == ":path"}).map( | entry|{ entry.1 });
+
+        let url =  (authority, path);
+        if let (Some(authority), Some(path)) = url {
+            let params = url::Url::parse(format!("http://{}{}", authority, path).as_str()).unwrap();
+            for (k, v) in params.query_pairs() {
+                if k.to_string() == "code" {
+                    return Some(v.to_string())
+                }
+            }
+            return None
+        }
+        None
+    }
+
+    fn create_token_request(&self, code: String) -> HttpRequest {
+        util::token_request(
+            &AuthType::RequestBody,
+            &(self.config.client_id),
+            Some(&self.config.client_secret),
+            &[],
+            Some(&RedirectUrl::from_url(self.config.redirect_url.clone())),
+            None,
+            &TokenUrl::from_url(self.config.token_url.clone()),
+            vec![("grant_type", "authorization_code"), ("code", code.as_str())])
+    }
 }
 
 impl State for Start  {
@@ -137,21 +182,47 @@ impl State for Start  {
     fn handle_request(&self, oauther: &OAuther, headers: &Vec<(&str, &str)>) -> Response {
         // check cookie
         match oauther.session_cookie(headers) {
-            Some(cookie) => NewState(Box::new(CookieFound { })),
+            Some(_) => NewState(Box::new(SessionCookiePresent { })),
             None => {
-                let (url, update) = oauther.authorization_server_redirect();
-                let mut headers = HeaderMap::new();
-                headers.insert(SET_COOKIE, oauther.create_session_cookie().to_string().parse().unwrap());
-                NewAction(OAutherAction::Redirect(url, headers,  update))
+                Response::NewState(Box::new(NoValidSession { }))
             },
         }
     }
 }
 
-impl State for CookieFound {
+impl State for NoValidSession {
+    fn handle_request(&self, oauther: &OAuther, _: &Vec<(&str, &str)>) -> Response {
+        let (url, update) = oauther.authorization_server_redirect();
+        let mut headers = HeaderMap::new();
+        headers.insert(SET_COOKIE, oauther.create_session_cookie().to_string().parse().unwrap());
+        NewAction(OAutherAction::Redirect(url, headers,  update))
+    }
+}
 
-    fn handle_request(&self, _oauther: &OAuther, _header: &Vec<(&str, &str)>) -> Response {
-        NewAction(OAutherAction::Noop)
+impl State for SessionCookiePresent {
+    fn handle_request(&self, oauther: &OAuther, headers: &Vec<(&str, &str)>) -> Response {
+        let session = oauther.session_cookie(headers)
+            .expect("Bad state error, in SessionCookiePresent state, but no cookie found");
+        let tokens = oauther.cache.get_tokens_for_session(&session);
+        match tokens {
+            None => {
+                let code = oauther.request_auth_code(headers);
+                match code {
+                    None => Response::NewState( Box::new(NoValidSession {})),
+                    Some(code) => {
+                        // TODO include PKCE
+                        let request = oauther.create_token_request(code);
+                        Response::NewAction(OAutherAction::HttpCall(request))
+                    }
+                }
+
+            }
+            Some(tokens) => {
+                // TODO maybe validate expiry? But it is probably RS responsibility
+                // TODO fix hack where we just take the first token in the list
+                Response::NewAction(OAutherAction::Allow(oauther.allow_headers(&tokens[0])))
+            }
+        }
     }
 }
 
@@ -164,12 +235,13 @@ enum Response {
 enum OAutherAction {
     Noop,
     Redirect(Url, HeaderMap, Box<dyn Fn(&mut OAuther) -> ()>),
-    HttpCall,
-    Allow
+    HttpCall(HttpRequest),
+    Allow(HeaderMap)
 }
 
 struct Start { }
-struct CookieFound {  }
+struct NoValidSession { }
+struct SessionCookiePresent {  }
 
 struct OAutherConfig {
     cookie_name: String,
@@ -207,6 +279,8 @@ mod tests {
     use crate::oauther::OAutherAction::Redirect;
     use std::borrow::Borrow;
     use std::collections::HashMap;
+    use oauth2::AccessToken;
+    use oauth2::http::header::FORWARDED;
 
     struct TestCache {
         sessions: HashMap<String, Vec<String>>,
@@ -267,6 +341,27 @@ mod tests {
     }
 
     #[test]
+    fn session_cookie() {
+        let test_headers = vec![("cookie", "sessioncookie=value")];
+        let oauther= test_oauther();
+        assert_eq!(oauther.session_cookie(&test_headers).unwrap(), "value");
+    }
+
+    #[test]
+    fn auth_code_header() {
+        let oauther= test_oauther();
+        let authority = oauther.config.authorization_url.origin().unicode_serialization();
+        let test_headers = vec![
+            ("cookie", "sessioncookie=mysession"),
+            (":path", "auth/?code=awesomecode&state=state123"),
+            (":authority", authority.as_str())
+        ];
+        assert_eq!(oauther.request_auth_code(&test_headers).unwrap(), "awesomecode");
+    }
+
+
+
+    #[test]
     fn unauthorized_request() {
         let mut oauther = test_oauther();
 
@@ -282,11 +377,45 @@ mod tests {
     }
 
     #[test]
-    fn session_cookie_present_request() {
+    fn session_cookie_present_but_no_token_in_cache_request() {
         let mut oauther= test_oauther();
-        let action = oauther.handle_request_header(vec![("sessioncookie", "value")]);
-        assert_eq!(action.type_id(), Action::Allow.type_id());
+        let action = oauther.handle_request_header(vec![("cookie", "sessioncookie=value")]);
+        if let Action::Redirect(url, headers) = action {
+            assert_eq!(url.origin().unicode_serialization().as_str(), "http://authorization");
+        } else {panic!("actions was not redirect")}
     }
+
+    #[test]
+    fn session_cookie_present_and_valid_token_in_cache_request() {
+        let mut oauther= test_oauther();
+        oauther.cache
+            .set_tokens_for_session(
+                "mysession".to_string(),
+                vec!["OpaqueTestToken".to_string()]
+            );
+
+        let action = oauther.handle_request_header(vec![("cookie", "sessioncookie=mysession")]);
+        if let Action::Allow( headers ) = action {
+            assert!(headers.contains_key(AUTHORIZATION))
+        } else {panic!("action should be to allow")}
+    }
+
+    #[test]
+    fn session_cookie_present_no_valid_token_in_cache_but_auth_code_in_query() {
+        let mut oauther= test_oauther();
+
+
+        let action = oauther.handle_request_header(
+            vec![
+                ("cookie", "sessioncookie=mysession"),
+                (":path", "auth/?code=awesomecode&state=state123"),
+                (":authority", oauther.config.authorization_url.origin().unicode_serialization().as_str())
+            ]);
+        if let Action::HttpCall( http_request ) = action {
+            assert_eq!(http_request.url.as_str(), oauther.config.token_url.as_str())
+        } else {panic!("action should be to HttpCall")}
+    }
+
 
     #[test]
     fn code_grant_redirect() {
